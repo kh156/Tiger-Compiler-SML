a5d5f2b2b54a3348c046524392b7453dbdfaef56
kh156 <kuanghan.kevin@gmail.com>
2014-04-03 23:18:13 -0400

    .

--- instrSelection/test.tig.s
+++ instrSelection/test.tig.s
@@ -1,7 +1,13 @@
-L4:
+L5:
 li t105, 5
 move t129, t105
-li t106, 0
-move t101, t106
-j L3
-L3:
+move t106, t129
+jal L3
+move t101, t101
+j L4
+L4:
+L7:
+addi t107, t130, 1
+move t101, t107
+j L6
+L6:
