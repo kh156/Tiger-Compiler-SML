1b6865baf1e9c1dbf6eb9a40a8a2290fa547864a
Ang-Li-al171 <la.imagine@hotmail.com>
2014-04-03 23:37:23 -0400

    fixed forloop jump

--- instrSelection/test.tig.s
+++ instrSelection/test.tig.s
@@ -1,13 +1,22 @@
-L59:
-la t259, L57
-move t249, t259
-la t260, L56
-move t248, t260
-move t262, t248
-move t263, t249
-jal stringEqual
-move t101, t261
-j L58
-L58:
-L57: xyz
-L56: abc
+L69:
+li t294, 10
+li t295, 4
+mult t293, t294, t295
+move t292, t293
+jal malloc
+move t280, t291
+li t298, 10
+move t297, t298
+li t300, 0
+move t299, t300
+jal initArray
+move t281, t280
+li t305, 2
+addi t304, t305, ~1
+li t306, 4
+mult t303, t304, t306
+add t302, t281, t303
+lw t301, 0(t302)
+move t101, t301
+j L68
+L68:
